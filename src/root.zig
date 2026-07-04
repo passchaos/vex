fddf6b580052d591bf6482554dd42946da7f62e6
passchaos <greedwolf.dss@gmail.com>
2026-07-05 07:07:48 +0800

    Tighten Graphviz start-to-right path gate

--- src/root.zig
+++ src/root.zig
@@ -15866,7 +15866,7 @@ test "user cluster example stays compact and Graphviz-like" {
     try expectSvgEdgeControlsNear(svg, graphviz_oracle, "b3-&gt;end", 0.8, 0.8);
     try expectSvgEdgeControlsNear(svg, graphviz_oracle, "b2-&gt;b3", 1.0, 1.0);
     try expectSvgEdgePathPointsNear(svg, graphviz_oracle, "start-&gt;a0", 0.07);
-    try expectSvgEdgePathPointsNear(svg, graphviz_oracle, "start-&gt;b0", 0.08);
+    try expectSvgEdgePathPointsNear(svg, graphviz_oracle, "start-&gt;b0", 0.075);
     try expectSvgEdgePathPointsNear(svg, graphviz_oracle, "a0-&gt;a1", 0.045);
     try expectSvgEdgePathPointsNear(svg, graphviz_oracle, "a1-&gt;a2", 0.045);
     try expectSvgEdgePathPointsNear(svg, graphviz_oracle, "a2-&gt;a3", 0.045);
