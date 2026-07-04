752d7ff8f14fd51e1615d1378ed1e5f23899a3a3
passchaos <greedwolf.dss@gmail.com>
2026-07-04 17:00:09 +0800

    Cover left end edge oracle path

--- src/root.zig
+++ src/root.zig
@@ -15263,6 +15263,7 @@ test "user cluster example stays compact and Graphviz-like" {
     try expectSvgEdgePathPointsNear(svg, graphviz_oracle, "b1-&gt;b2", 1.6);
     try expectSvgEdgePathPointsNear(svg, graphviz_oracle, "b2-&gt;b3", 1.1);
     try expectSvgEdgePathPointsNear(svg, graphviz_oracle, "a1-&gt;b3", 2.7);
+    try expectSvgEdgePathPointsNear(svg, graphviz_oracle, "a3-&gt;end", 2.4);
     try expectSvgEdgePathPointsNear(svg, graphviz_oracle, "b3-&gt;end", 2.8);
     try expectSvgEdgeEndpointsNear(svg, graphviz_oracle, "b0-&gt;b1", 2.65);
     try expectSvgEdgeEndpointsNear(svg, graphviz_oracle, "b1-&gt;b2", 2.6);
