81be178d93447492f2abbf4b11ba889c424a63b4
passchaos <greedwolf.dss@gmail.com>
2026-07-04 17:32:06 +0800

    Tighten terminal node x oracle gates

--- src/root.zig
+++ src/root.zig
@@ -15141,8 +15141,8 @@ test "user cluster example stays compact and Graphviz-like" {
     try std.testing.expect(@abs(svgNodeScreenCenterX(svg, "b1").? - svgNodeScreenCenterX(graphviz_oracle, "b1").?) <= 1.3);
     try std.testing.expect(@abs(svgNodeScreenCenterX(svg, "b2").? - svgNodeScreenCenterX(graphviz_oracle, "b2").?) <= 1.0);
     try std.testing.expect(@abs(svgNodeScreenCenterX(svg, "b3").? - svgNodeScreenCenterX(graphviz_oracle, "b3").?) <= 2.55);
-    try std.testing.expect(@abs(svgNodeScreenCenterX(svg, "start").? - svgNodeScreenCenterX(graphviz_oracle, "start").?) <= 1.5);
-    try std.testing.expect(@abs(svgNodeScreenCenterX(svg, "end").? - svgNodeScreenCenterX(graphviz_oracle, "end").?) <= 1.5);
+    try std.testing.expect(@abs(svgNodeScreenCenterX(svg, "start").? - svgNodeScreenCenterX(graphviz_oracle, "start").?) <= 0.7);
+    try std.testing.expect(@abs(svgNodeScreenCenterX(svg, "end").? - svgNodeScreenCenterX(graphviz_oracle, "end").?) <= 0.7);
     try expectSvgNodeClusterPaddingNear(svg, graphviz_oracle, "cluster_0", "a0", 2.6);
     try expectSvgNodeClusterPaddingNear(svg, graphviz_oracle, "cluster_0", "a3", 2.6);
     try expectSvgNodeClusterPaddingNear(svg, graphviz_oracle, "cluster_1", "b0", 2.55);
